9438471067266352b4094de0ad030147dd20ce45
Nick Rasmussen <nlr30262g.rit.edu>
2020-09-14 22:56:04 -0400

    imageData will be the death of me

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 (function(){
     
-let ctx, canvas;
-const canvasWidth=640,canvasHeight=480;
+    let ctx, canvas;
+    const canvasWidth=640,canvasHeight=480;
+
+
+    window.onload = init;
+
+    function init(){
+        console.log("init");
+        canvas=document.querySelector("canvas");
+        ctx=canvas.getContext('2d');
+        canvas.width=canvasWidth;
+        canvas.height=canvasHeight;
+
+        ctx.fillRect(0,0,canvasWidth,canvasHeight);
+        
+        sandJS.init(ctx,canvas);
+        
+        setInterval(update, 1000/1);
+        //setInterval(function(){sandJS.createGrain(canvas.width/2,canvas.height/2);},300);
+        sandJS.createGrain(canvas.width/2,canvas.height-10);
+        sandJS.createGrain(canvas.width/2,canvas.height-11);
+    }  
     
     
-window.onload = init;
+    function update(){
+        cls();
+        
+        sandJS.updateSand();
+        
+        sandJS.drawSand();
+    }
     
-function init(){
-    console.log("init");
-    canvas=document.querySelector("canvas");
-    ctx=canvas.getContext('2d');
-    canvas.width=canvasWidth;
-    canvas.height=canvasHeight;
-    
-    ctx.fillRect(0,0,canvasWidth,canvasHeight);
-}  
+    function cls(){
+        cnmLib.drawRect(ctx,0,0,canvas.width,canvas.height);
+    }
 })();
---
+++ src/sand.js
@@ -0,0 +1,59 @@
+(function(){
+
+let sandJS = {
+    
+    sand: [],
+    ctx: [], //lil yucky but whatevs
+    canvas: [],
+    
+    init(pCTX,pCanvas){
+        sand = [];
+        ctx = pCTX;
+        canvas = pCanvas;
+    },
+
+    createGrain(pX,pY){
+        let grain = {
+            x: pX,
+            y: pY,
+            color: '#ffe100',
+            move(){
+                let imgData = ctx.getImageData(this.x,this.y,1,1);
+                console.log(imgData.data);
+                
+//                //check below
+//                if(imgData.data[0]==0 && imgData.data[1]==0 && imgData.data[2] == 0){
+//                    if(this.y < canvas.height - 1) this.y++;
+//                }
+//                
+//                //check left
+//                else{ /*if(imgData.data[0]==0 && imgData.data[1]==0 && imgData.data[2]==0)*/
+//                    this.x--;
+//                    if(this.y < canvas.height - 1) this.y++;
+//                    console.log("leeeeeeft");
+//                }
+            }
+        }
+        sand.push(grain);
+    },
+    
+    updateSand(){
+        for(let i = 0; i < sand.length; i++){
+            sand[i].move();
+        }
+    },
+    
+    drawSand(){
+        let imgData;
+        for(let i = 0; i < sand.length; i++){
+            //nightmare nightmare nightmare nightmare nightmare
+            ctx.save();
+            ctx.fillStyle = sand[i].color;
+            ctx.fillRect(sand[i].x,sand[i].y,1,1);      //pretty sure this can be better by drawing directly to imageData
+            ctx.restore();
+        }
+    }
+}
+if(window) window["sandJS"] = sandJS;
+else throw "window not defined";
+})();
